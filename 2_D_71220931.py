e041f0c18eeef19f316dba9ca7e3255820b48fba
Brilianmadiuw <117911653+Brilianmadiuw@users.noreply.github.com>
2022-11-24 17:15:53 +0700

    Add files via upload

---
+++ 2_D_71220931.py
@@ -0,0 +1,8 @@
+pertama = input("masukkan nama pemain pertama: ")
+kedua = input("masukkan nama pemain kedua: ")
+ketiga = input("masukkan nama pemain ketiga: ")
+i1 = int(input("masukkan jumlah kartu pemain pertama: "))
+i2 = int(input("masukkan jumlah kartu pemain kedua: "))
+i3 = int(input("masukkan jumlah kartu pemain ketiga: "))
+if (i1> i2 and i3):
+    print(pertama, "menang dengan jumlah kartu sebanyak", i1)
