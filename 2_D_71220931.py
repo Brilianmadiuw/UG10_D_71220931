7c0577e0b7fa27f565a5a70b0b56f5ec4db36d0f
Brilianmadiuw <117911653+Brilianmadiuw@users.noreply.github.com>
2022-11-24 17:22:35 +0700

    Add files via upload

--- 2_D_71220931.py
+++ 2_D_71220931.py
@@ -6,3 +6,14 @@ i2 = int(input("masukkan jumlah kartu pemain kedua: "))
 i3 = int(input("masukkan jumlah kartu pemain ketiga: "))
 if (i1> i2 and i3):
     print(pertama, "menang dengan jumlah kartu sebanyak", i1)
+elif (i2>i1 and i3):
+    print(kedua, "menang dengan jumlah kartu sebanyak", i2)
+elif (i3>i1 and i2):
+    print(ketiga, "menang dengan jumlah kartu sebanyak", i3)
+elif (i1>21):
+    print("jumlah kartu yang dimiliki melebihi batas")
+elif (i2>21):
+    print("jumlah kartu yang dimiliki melebihi batas")
+elif (i3>21):
+    print("jumlah kartu yang dimiliki melebihi batas")
+
